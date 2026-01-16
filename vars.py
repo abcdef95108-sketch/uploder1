e26ec3b7d8fc944fffac403b58971585ed7c0fef
abcdef95108-sketch <abcdef95108@gmail.com>
2026-01-16 15:57:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "13076045"))
 API_HASH = os.environ.get("API_HASH", "32f2036dcb66c94637a7e87de6b6001b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8469714434:AAHGrS-I_qGYN5NhLgiHgMUXEUzOo4LDH1g")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8204407217:AAGpK7Z1pZGNyoJ7PAsEVESDuPJ7z7P-sKs")
 
 CREDIT = os.environ.get("CREDIT", "𝐈𝐓'𝐬𝐆𝐎𝐋𝐔")
 # MongoDB Configuration
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipT
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "8222519141"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "8222519141").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8478103699"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8478103699").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = ""
@@ -59,6 +59,7 @@ Please contact the admin to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
